1cfd17d5eadfdf4b82451510477fc17c1024999d
sandeep1404-praj <prajapatisandeep1404@gmail.com>
2025-09-05 00:53:35 +0530

    cors

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # frontend origin
+    allow_origins=["dailybot.netlify.app"],  # frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
